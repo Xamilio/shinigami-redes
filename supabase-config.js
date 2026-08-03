39146b1e495c604bbd337e5055282f5d2b5fe985
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2026-08-04 01:36:20 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,9 +1,3 @@
-
-const SUPABASE_URL = 'https://azypglollvjyjuizfcxz.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6eXBnbG9sbHZqeWp1aXpmY3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NDA0MzMsImV4cCI6MjA5MDExNjQzM30.bAdC9sU_K14WEo4F4v21Fsx0g4MF4YsmXUrLDS2DU9A';
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 window.supabaseClient = supabaseClient;
 window.resolveImage = function(path) {
     if (!path) return '';
